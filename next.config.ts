c7b7ed3ccf53b9cbf21c11d965e4e9c32398b003
Hoang Anh <hoanganh.luong2906@gmail.com>
2024-10-26 08:50:04 +0700

    :bug:[fix]: Another effort

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import { Header } from 'next/dist/lib/load-custom-routes';
 
 const cspHeader = `
     default-src 'self';
@@ -17,22 +18,20 @@ const nextConfig: NextConfig = {
 	sassOptions: {
 		implementation: 'sass-embedded',
 	},
-};
-
-module.exports = {
-	async headers() {
-		return [
-			{
-				source: '/(.*)',
-				headers: [
-					{
-						key: 'Content-Security-Policy',
-						value: cspHeader.replace(/\n/g, ''),
-					},
-				],
-			},
-		];
-	},
+	headers: async () => [
+		{
+			key: 'Content-Security-Policy',
+			value: `
+			default-src 'self'; 
+			script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' 'chrome-extension://9d50b3b1-7d63-4009-9b1b-3bf8d973d69e';	
+			script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' 'chrome-extension://572a7de1-05c9-48da-b6e8-1a46d3a257be';
+			style-src 'self' https://trustedstyles.com; 
+			img-src 'self' data: https://trustedimages.com; 
+			connect-src 'self'; 
+			frame-src 'self' https://trustedframes.com;
+		  `.replace(/\n/g, ''), // Loại bỏ dòng trắng để CSP không bị ngắt
+		} as unknown as Header,
+	],
 };
 
 export default nextConfig;
