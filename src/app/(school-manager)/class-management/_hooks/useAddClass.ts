832296956f113e954b4172454d301f88ee710538
mainht <112955345+mainht@users.noreply.github.com>
2024-11-12 13:18:07 +0700

    👽️ Update add class api props

--- src/app/(school-manager)/class-management/_hooks/useAddClass.ts
+++ src/app/(school-manager)/class-management/_hooks/useAddClass.ts
@@ -6,22 +6,24 @@ interface IAddClassProps {
   schoolId: string;
   sessionToken: string;
   formData: IAddClassData[];
+  schoolYearId: number;
 }
 
 const useAddClass = async (props: IAddClassProps) => {
-    const { schoolId, formData, sessionToken } = props;
-    try {
-        const response = await addClass(schoolId, sessionToken, formData[0]);
-        useNotify({
-          message: 'Thêm lớp học thành công',
-          type: 'success',
-        });
-        return response;
-      } catch (err: any) {
-        useNotify({
-          message: 'Thêm lớp học thất bại. Vui lòng thử lại.',
-          type: 'error',
-        });
-      }
+  const { schoolId, formData, sessionToken, schoolYearId } = props;
+  try {
+      const response = await addClass(schoolId, sessionToken, formData[0], schoolYearId);
+      useNotify({
+        message: 'Thêm lớp học thành công',
+        type: 'success',
+      });
+      return response;
+    } catch (err: any) {
+      useNotify({
+        message: 'Thêm lớp học thất bại. Vui lòng thử lại.',
+        type: 'error',
+      });
+    }
 }
+
 export default useAddClass;
