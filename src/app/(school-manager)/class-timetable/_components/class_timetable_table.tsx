ffef0129aac34a09410d171790386123de26824c
mainht <112955345+mainht@users.noreply.github.com>
2024-12-10 17:19:42 +0700

    ✨ [feat]: Get data class timetable

--- src/app/(school-manager)/class-timetable/_components/class_timetable_table.tsx
+++ src/app/(school-manager)/class-timetable/_components/class_timetable_table.tsx
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
+import { useAppContext } from "@/context/app_provider";
 import {
+  CLASSGROUP_TRANSLATOR,
+  IClassPeriod,
+  IClassSchedule,
+  ITermResponse,
+  TIMETABLE_SLOTS,
+  WEEK_DAYS_FULL
+} from "@/utils/constants";
+import {
+  FormControl,
+  MenuItem,
+  Select,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
-  TableRow,
+  TableRow
 } from "@mui/material";
-import {
-  IScheduleResponse,
-  TIMETABLE_SLOTS,
-  WEEK_DAYS_FULL,
-} from "@/utils/constants";
-
+import { DatePicker } from "@mui/x-date-pickers";
+import dayjs, { Dayjs } from "dayjs";
+import { useEffect, useMemo, useState } from "react";
+import useClassTimetable from "../_hooks/useClassTimetable";
+import { getTerms } from "../_libs/apiClassTimetable";
 interface ClassTimetableTableProps {
-  scheduleData: IScheduleResponse | null;
-  selectedClass: string;
+  schoolId: string;
+  schoolYearId: number;
+  sessionToken: string;
 }
 
 const headerCellStyle = {
@@ -46,13 +57,160 @@ const periodCellStyle = (hasPeriod: boolean) => ({
   },
 });
 
+const ITEM_HEIGHT = 40;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+      scrollbars: "none",
+    },
+  },
+};
 
 export default function ClassTimetableTable({
-  scheduleData,
-  selectedClass,
+  schoolId,
+  schoolYearId,
+  sessionToken,
 }: ClassTimetableTableProps) {
+  const [selectedTerm, setSelectedTerm] = useState<number>(1);
+  const [terms, setTerms] = useState<ITermResponse[]>([]);
+  const { selectedSchoolYearId } = useAppContext();
+  const [selectedDate, setSelectedDate] = useState<Dayjs>(dayjs());
+  const [selectedTeacher, setSelectedTeacher] = useState("all");
+  const [selectedClass, setSelectedClass] = useState<string>("");
+  const [selectedGrade, setSelectedGrade] = useState<string>("");
+
+  useEffect(() => {
+    const fetchTerm = async () => {
+      try {
+        const response = await getTerms(sessionToken, selectedSchoolYearId);
+        if (response.status === 200) {
+          setTerms(response.result.items);
+        }
+      } catch (error) {
+        console.error("Failed to fetch term name:", error);
+      }
+    };
+    fetchTerm();
+  }, [sessionToken, selectedSchoolYearId]);
+
+  const {
+    data: scheduleData,
+    error,
+    isValidating,
+  } = useClassTimetable({
+    schoolId,
+    schoolYearId,
+    termId: selectedTerm,
+    sessionToken,
+    date: selectedDate.toDate(),
+  });
+
+  const classNames = useMemo(() => {
+    if (!scheduleData?.["class-schedules"]) return [];
+
+    const names =
+      selectedGrade === "all"
+        ? scheduleData["class-schedules"].map(
+            (schedule: IClassSchedule) => schedule["student-class-name"]
+          )
+        : scheduleData["class-schedules"]
+            .filter((schedule: IClassSchedule) =>
+              schedule["student-class-name"].startsWith(
+                String(CLASSGROUP_TRANSLATOR[selectedGrade])
+              )
+            )
+            .map((schedule: IClassSchedule) => schedule["student-class-name"]);
+
+    return Array.from(new Set(names)) as string[];
+  }, [scheduleData, selectedGrade]);
+
+  const teacherNames = useMemo(() => {
+    if (!scheduleData) return [];
+    const teachers = new Set<string>();
+
+    scheduleData["class-schedules"].forEach((schedule: IClassSchedule) => {
+      schedule["class-periods"].forEach((period: IClassPeriod) => {
+        teachers.add(period["teacher-abbreviation"]);
+      });
+    });
+
+    return Array.from(teachers) as string[];
+  }, [scheduleData]);
+
   return (
     <div className="w-full h-[90vh] flex flex-col justify-start items-center pb-[2vh]">
+      <div className="w-full mb-6 flex justify-between items-center gap-4">
+        <div className="flex gap-4">
+          <FormControl sx={{ minWidth: 170 }}>
+            <Select
+              value={selectedTerm}
+              onChange={(e) => setSelectedTerm(Number(e.target.value))}
+              variant="standard"
+            >
+              {terms.map((term) => (
+                <MenuItem key={term.id} value={term.id}>
+                  {term.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <DatePicker
+            value={selectedDate}
+            onChange={(newValue) => setSelectedDate(newValue || dayjs())}
+            format="DD/MM/YYYY"
+            slotProps={{ textField: { variant: "standard" } }}
+          />
+          <FormControl sx={{ minWidth: 170 }}>
+            <Select
+              value={selectedGrade}
+              onChange={(e) => setSelectedGrade(e.target.value)}
+              variant="standard"
+            >
+              <MenuItem value="">Chọn khối</MenuItem>
+              {Object.entries(CLASSGROUP_TRANSLATOR).map(([key, value]) => (
+                <MenuItem key={key} value={key}>
+                  {`Khối ${value}`}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl sx={{ minWidth: 170 }}>
+            <Select
+              value={selectedClass}
+              onChange={(e) => setSelectedClass(e.target.value)}
+              variant="standard"
+              MenuProps={MenuProps}
+            >
+              <MenuItem value="">Chọn lớp</MenuItem>
+              {classNames.map((className: string) => (
+                <MenuItem key={className} value={className}>
+                  {className}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl sx={{ minWidth: 170 }}>
+            <Select
+              value={selectedTeacher}
+              onChange={(e) => setSelectedTeacher(e.target.value)}
+              variant="standard"
+              MenuProps={MenuProps}
+            >
+              <MenuItem value="all">Tất cả giáo viên</MenuItem>
+              {teacherNames.map((teacher) => (
+                <MenuItem key={teacher} value={teacher}>
+                  {teacher}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+
       <TableContainer
         sx={{ mb: 10, maxHeight: "100%" }}
         className="!no-scrollbar"
@@ -89,10 +247,21 @@ export default function ClassTimetableTable({
                     const currentSlotIndex =
                       dayIndex * 10 + sessionIndex * 5 + slotIndex + 1;
                     const period = scheduleData?.["class-schedules"]
-                      .find((s) => s["student-class-name"] === selectedClass)
+                      .filter((s: IClassSchedule) =>
+                        s["student-class-name"].startsWith(
+                          String(CLASSGROUP_TRANSLATOR[selectedGrade])
+                        )
+                      )
+                      .find(
+                        (s: IClassSchedule) =>
+                          s["student-class-name"] === selectedClass
+                      )
                       ?.["class-periods"].find(
-                        (p) =>
-                          p["start-at"] === currentSlotIndex && !p["is-deleted"]
+                        (p: IClassPeriod) =>
+                          p["start-at"] === currentSlotIndex &&
+                          !p["is-deleted"] &&
+                          (selectedTeacher === "all" ||
+                            p["teacher-abbreviation"] === selectedTeacher)
                       );
 
                     return (
@@ -105,9 +274,14 @@ export default function ClassTimetableTable({
                             <strong className="tracking-wider text-ellipsis text-nowrap overflow-hidden text-primary-500 text-sm font-semibold">
                               {period["subject-abbreviation"]}
                             </strong>
-                            <p className="text-ellipsis text-nowrap overflow-hidden text-gray-600 text-xs">
-                              {period["teacher-abbreviation"]}
-                            </p>
+                            <div className="flex justify-between w-full">
+                              <p className="text-ellipsis text-nowrap overflow-hidden text-gray-600 text-xs">
+                                {period["room-code"]}
+                              </p>
+                              <p className="text-ellipsis text-nowrap overflow-hidden text-gray-600 text-xs">
+                                {period["teacher-abbreviation"]}
+                              </p>
+                            </div>
                           </div>
                         )}
                       </TableCell>
